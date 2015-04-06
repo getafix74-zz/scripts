eabe494591a073760f4b295287396d810ee13f9f
getafix74 <steveherron74@gmail.com>
2015-04-06 22:33:27 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # scripts
+
+This is just a collection of scripts that I've found useful or interesting. Some my just be solutions to problems, challenges or wargames that I've done.
---
+++ greetings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+"""
+This is my attempt at the script that appears in the NullByte article:
+http://null-byte.wonderhowto.com/how-to/hack-like-pro-python-scripting-for-aspiring-hacker-part-1-0159118/
+
+It doesn't do anything spectacular and will probably be deleted. I'm using it as good practice for working with git and github
+
+Steve H 06/04/15
+
+"""
+
+name="Steve"
+print "Greetings, "+name+" from Nullbytes"
